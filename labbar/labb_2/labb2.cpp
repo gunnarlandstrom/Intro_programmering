6d9c331c2d5012e0df2eb1dc8f236a964fb4fef5
Gunnar <gunnar.landstrom@hotmail.com>
2024-11-27 10:28:08 +0100

    labb2_complete

--- labbar/labb_2/labb2.cpp
+++ labbar/labb_2/labb2.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+/*
+    Gunnar Landström
+    29/11-2024
+    DT028G Datateknik GR (A), Introduktion till programmering
+    Labb_2
+*/
 
+#include <fstream>
 int main() {
     std::ifstream namesInput("names.txt");
     std::ofstream namesOutput("namesOutput.txt");
@@ -9,30 +13,37 @@ int main() {
     std::string lastName;
     std::string placeOfResidence;
     std::string socialSecurityNumber;
-    
+    std::string whatGender;
+    std::string socialSecurityGender;
+
+    // 
     while( !namesInput.eof() ) {
 
 
         namesInput >> firstName >> lastName >> socialSecurityNumber;
+        socialSecurityGender = socialSecurityNumber.substr(0, 9);
+
+        // Kollar om det är man eller kvinna
+        if ( std::stoi(socialSecurityGender) % 2 == 0 ) {
+            whatGender = "[F]";
+        }
+        else {
+            whatGender = "[M]";
+        }
         
-        if ( namesInput.eof()) {
+        if ( namesInput.eof() ) {
             break;
         }
 
         std::getline(namesInput, placeOfResidence);
         std::getline(namesInput, placeOfResidence);
 
-        namesOutput << lastName << " " << firstName << " \n" <<  socialSecurityNumber << "\n" << placeOfResidence << "\n";
+        namesOutput << lastName << " " << firstName << " " << whatGender << "\n" << placeOfResidence << "\n" << "\n";
 
     }
 
     namesInput.close();
     namesOutput.close();
-
-    std::cout << lastName << " ";
-    std::cout << firstName << " \n";
-    std::cout << socialSecurityNumber << " \n";
-    std::cout << placeOfResidence << " \n";
 
 return 0;
 }
